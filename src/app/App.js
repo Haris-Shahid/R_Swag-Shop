67a738e2b8abe7c391717f52b33f81db12ee5c0b
Haris Shahid <harisshahid01@yahoo.com>
2018-06-01 17:13:19 +0500

    swagshop: 1st product component for website

--- src/app/App.js
+++ src/app/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HttpService from "../services/http-services";
+import Product from '../product/product';
 
 const http = new HttpService();
 
@@ -27,9 +28,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-main" >
+          <Product />
+        </div>
       </div>
     );
   }
